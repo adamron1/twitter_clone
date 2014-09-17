14a6a9a3c815a5c16d2e2771840c0248ca549eb1
Adam Ron <apprentice@Fiji.local>
2014-09-17 12:15:29 +1200

    Added and tested User validation - max user_name length of 20 chars

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,18 @@ RSpec.describe User, :type => :model do
       expect(user.valid?).to eq(true)
     end
 
-    it "rejects a user with a short password (DEVISE)" do
+    it "rejects a new user with a short password (DEVISE)" do
       user = User.new(user_name: "mr_cat", email: "puss@boots.com", password:"cats")
       expect(user.valid?).to eq(false)
     end
 
-    it "rejects a user with a short username" do
+    it "rejects a new user with a short username (under 4 characters)" do
       user = User.new(user_name: "cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
+      expect(user.valid?).to eq(false)
+    end
+
+    it "rejects a new user with a long username (over 20 characters)" do
+      user = User.new(user_name: "mr_really_big_and_strong_cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
       expect(user.valid?).to eq(false)
     end
 
