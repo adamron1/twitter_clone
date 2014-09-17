e3500d7bfd7c206e1c0b744c6fe805284cdf1c36
Adam Ron <apprentice@Fiji.local>
2014-09-17 12:12:16 +1200

    Added and tested user validation for user_name - minimum of 4 chars

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  # describe "Initialise and validate" do
-  #   it "creates a new user" do
-  #     user = User.new(user_name: "mr_cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
-  #     expect(user.user_name).to eq("mr_cat")
-  #   end
-  # end
+  describe "Initialise" do
+    it "creates a new user" do
+      user = User.new(user_name: "mr_cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
+      expect(user.user_name).to eq("mr_cat")
+    end
+  end
+
+  describe "Validations" do
+
+    it "creates a valid user" do
+      user = User.new(user_name: "mr_cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
+      expect(user.valid?).to eq(true)
+    end
+
+    it "rejects a user with a short password (DEVISE)" do
+      user = User.new(user_name: "mr_cat", email: "puss@boots.com", password:"cats")
+      expect(user.valid?).to eq(false)
+    end
+
+    it "rejects a user with a short username" do
+      user = User.new(user_name: "cat", email: "puss@boots.com", password:"catsrule&dogsdrool")
+      expect(user.valid?).to eq(false)
+    end
+
+  end
 
 end
