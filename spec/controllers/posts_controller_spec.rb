7f210db5c023a70ef3dc0fb0c1c2d44707fd5be2
Adam Ron <apprentice@Fiji.local>
2014-09-18 14:21:47 +1200

    Post #index works

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe PostsController, :type => :controller do
 
-  describe "#index" do
-    pending
+  describe "GET index" do
+    it "redirects to the posts index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "stores all the posts in an instance variable" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
   describe "#show" do
